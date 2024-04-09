375129b3b933b3528b9bc85a558b0f9f7f6fc42b
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-04-09 20:26:20 +0100

    Bugs still exist
    Phomeme splitting will be required.

--- ahd2bepa/index.js
+++ ahd2bepa/index.js
@@ -12,7 +12,9 @@ o͝o;ʊ
 o͞o;uː
 ōō;uː
 ou;aʊ
-ä;ɑ
+ə;ʌ
+ər;ə
+ä;ɑː
 ă;æ
 ā;eɪ
 ĕ;ɛ
@@ -23,16 +25,22 @@ ou;aʊ
 ô;ɔː
 ō;əʊ
 ŭ;ʌ
-û;ɜː`;
+û;ɜː
+a;æ
+e;ɛ
+i;ɪ
+o;ɒ
+ʌr;ə`;
 dictText.split("\n").forEach((e) => {
 	let line = e.split(";");
 	dict[line[0]] = [line[1]];
 });
 
 let toBEPA = function (ahdText) {
-	let result = ahdText;
+	let result = ahdText.replaceAll("-", "");
 	for (let ahd in dict) {
 		result = result.replaceAll(ahd, dict[ahd]);
+		//console.debug(result);
 	};
 	return result;
 };
