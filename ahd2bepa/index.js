b6d0199e4c0a0b715a88cef4638dffd87edd35a8
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-04-09 20:10:22 +0100

    Update index.js

--- ahd2bepa/index.js
+++ ahd2bepa/index.js
@@ -2,7 +2,37 @@
 
 "use strict";
 
-let map = {}, mapText = `ä;ɑ
+let dict = {}, dictText = `o͝or;ʊə
+ŏŏr;ʊə
+âr;ɛə
+îr;ɪə
+oi;ɔɪ
+o͝o;ʊ
+ŏŏ;ʊ
+o͞o;uː
+ōō;uː
+ou;aʊ
+ä;ɑ
 ă;æ
 ā;eɪ
-âr;ɛə`;
+ĕ;ɛ
+ĕ;iː
+ĭ;ɪ
+ī;aɪ
+ŏ;ɒ
+ô;ɔː
+ō;əʊ
+ŭ;ʌ
+û;ɜː`;
+dictText.split("\n").forEach((e) => {
+	let line = e.split(";");
+	dict[line[0]] = [line[1]];
+});
+
+let toBEPA = function (ahdText) {
+	let result = ahdText;
+	for (let ahd in dict) {
+		result = result.replaceAll(ahd, dict[ahd]);
+	};
+	return result;
+};
